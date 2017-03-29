80fe2b5e914799177764ac09592528684c4215ef
graham.monteith <graham.monteith@selectcomfort.com>
2017-03-29 11:13:03 -0500

    Migrating to SN repo

---
+++ js/answer.js
@@ -0,0 +1,33 @@
+
+/**
+ * Given an array of Person objects, returns the root PersonTreeNode (the CEO).
+ * @param {Person[]} employees - An array of Person objects representing all the employees of the company.
+ * @returns {PersonTreeNode} The CEO of the organization.
+ */
+function generateTree(employees) {
+
+  /**
+   * @ignore
+   * INSTRUCTIONS:
+   * 1. ONLY edit this function and nothing else!.
+   *
+   * 2. Analyze the Person.js and PersonTreeNode.js files.
+   *
+   * 3. Parse the `employees` array and create a single PersonTreeNode
+   *    object representing the CEO (the Person with no `manager`).
+   *    All PersonTreeNode object's `directReports` arrays should contain
+   *    PersonTreeNode's for their direct reports...creating a tree.
+   *
+   * 4. Refresh or click the 'Retry Test' button to rerun the test.
+   *
+   *  Feel free to create any additional functions in this file as needed.
+   */
+
+  var ceo = null; // Should be a PersonTreeNode object at the end;
+
+  // YOUR CODE STARTS HERE
+
+  // YOUR CODE ENDS HERE
+
+  return ceo;
+};
